972de7c98b00737dc06d0a0586940c4076a4bbce
ailenpinoni4 <ailenpinoni4@gmail.com>
2025-09-08 11:12:18 -0300

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Script para formulario de contacto
 document.getElementById('contactForm').addEventListener('submit', function(e) {
   e.preventDefault();
-  alert("¡Gracias por contactarnos! 🌈\nTe responderemos en menos de 24hs.");
-  this.reset();
-});
+  alert("¡Gracias por contactarnos! 🖤\nTe responderemos en menos de 2
